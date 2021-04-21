bd57ff6fba02cd7b4d836972710ac3a7e3c25cee
jens123567 <jens.malmqvist03@gmail.com>
2021-04-21 14:58:08 +0200

    Pseudokod

--- slutProjekt/Program.cs
+++ slutProjekt/Program.cs
@@ -6,7 +6,23 @@ namespace slutProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //programet presenteras
+
+            //muvudmeny visas
+
+            //scen 1 presenteras med två val
+
+            //scen 2.1 presenteras med ytterligare två val
+            //scen 2.2 presenteras med ytterligare två val
+
+            //scen 3.1 presenteras med ytterligare två val
+            //scen 3.2 presenteras med ytterligare två val
+            //scen 3.3 presenteras med ytterligare två val
+            //scen 3.4 presenteras med ytterligare två val
+
+            //alternativen leder tillbaka till en avslutande händelse och sammanfattning
+
+            //huvudmeny igen val att börja om eller stänga programmet
         }
     }
 }
