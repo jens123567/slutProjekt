c916fdf0c2e668eeb71daea4aaa61f7331ed6654
jens123567 <jens.malmqvist03@gmail.com>
2021-05-05 14:56:15 +0200

    kod comit

--- slutProjekt/Program.cs
+++ slutProjekt/Program.cs
@@ -6,23 +6,120 @@ namespace slutProjekt
     {
         static void Main(string[] args)
         {
+
             //programet presenteras
+            Console.WriteLine("Hej och välkommen till detta berättelse spel");
 
-            //muvudmeny visas
+            string spelaAvsluta = "";
+            //loop som allt ligger i
+            while (spelaAvsluta != "2")
+            {
+                //deklarera variabler
+                string tillScen3 = "";
+                string tillSlutscen = "";
 
-            //scen 1 presenteras med två val
+                //muvudmeny visas
+                huvudMeny();
+                spelaAvsluta = Console.ReadLine();
 
-            //scen 2.1 presenteras med ytterligare två val
-            //scen 2.2 presenteras med ytterligare två val
+                //scen 1 presenteras med två val
+                Console.WriteLine("text för scen 1");
+                Console.WriteLine("1. alternativ 1");
+                Console.WriteLine("2. alternativ 2");
+                string tillScen2 = Console.ReadLine();
 
-            //scen 3.1 presenteras med ytterligare två val
-            //scen 3.2 presenteras med ytterligare två val
-            //scen 3.3 presenteras med ytterligare två val
-            //scen 3.4 presenteras med ytterligare två val
+                // scen 2
+                switch (tillScen2)
+                {
+                    //scen 2.1 presenteras med ytterligare fyra val
+                    case "1":
+                        Console.WriteLine("text för scen 2.1");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        Console.WriteLine("3. alternativ 2");
+                        Console.WriteLine("4. alternativ 2");
+                        tillScen3 = Console.ReadLine();
+                        break;
 
-            //alternativen leder tillbaka till en avslutande händelse och sammanfattning
+                    //scen 2.2 presenteras med ytterligare fyra val
+                    case "2":
+                        Console.WriteLine("text för scen 2.2");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        Console.WriteLine("3. alternativ 2");
+                        Console.WriteLine("4. alternativ 2");
+                        tillScen3 = Console.ReadLine();
+                        break;
 
-            //huvudmeny igen val att börja om eller stänga programmet
+                    default:
+                        break;
+                }
+
+                switch (tillScen3)
+                {
+                    //scen 3.1 presenteras med ytterligare två val
+                    case "1":
+                        Console.WriteLine("text för scen 3.1");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        tillSlutscen = Console.ReadLine();
+                        break;
+
+                    //scen 3.2 presenteras med ytterligare två val
+                    case "2":
+                        Console.WriteLine("text för scen 3.2");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        tillSlutscen = Console.ReadLine();
+                        break;
+
+                    //scen 3.3 presenteras med ytterligare två val
+                    case "3":
+                        Console.WriteLine("text för scen 3.3");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        tillSlutscen = Console.ReadLine();
+                        break;
+
+                    //scen 3.4 presenteras med ytterligare två val
+                    case "4":
+                        Console.WriteLine("text för scen 3.4");
+                        Console.WriteLine("1. alternativ 1");
+                        Console.WriteLine("2. alternativ 2");
+                        tillSlutscen = Console.ReadLine();
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+                //alternativen leder tillbaka till en avslutande händelse och sammanfattning
+                switch (tillSlutscen)
+                {
+                    //du lyckades
+                    case "1":
+                        break;
+
+                    //du misslyckades
+                    case "2":
+                        break;
+
+                    default:
+                        break;
+                }
+
+                //huvudmeny igen val att börja om eller stänga programmet
+                Console.WriteLine("spelet är slut vill du spela igen?");
+                huvudMeny();
+                spelaAvsluta = Console.ReadLine();
+            }
+        }
+        static void huvudMeny()
+        {
+            Console.WriteLine("Välj ett alternativ");
+            Console.WriteLine("1. Starta spelet");
+            Console.WriteLine("2. Avsluta");
         }
     }
 }
